4b31bf8b0c607d3c881c170eacc9b51e1baec532
cplasabas <cedric@futuristech.ph>
2018-03-02 18:49:30 +0800

    Recommendation

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Models\User;
 use App\Models\Order;
 use App\Models\Product;
+use App\Models\Promotion;
 use Illuminate\Support\Facades\DB;
 
 Route::get('/', function () {
@@ -14,6 +15,30 @@ Route::get('/', function () {
                 ->groupBy('orders.user_id', 'products.name')
                 ->get();
     $products = Product::limit(10)->get();
+    $promotions = Promotion::all();
 
     return view('index')->with('users',$users)->with('orders',$orders)->with('products',$products);
+});
+
+
+Route::get('/recommend', function () {
+
+	ob_start();
+	system('cd c:/flink/bin && flink run c:/mcx.jar', $return);
+	ob_clean();
+
+	$users = User::all();
+	$orders = DB::table('orders')
+                ->select(DB::raw('orders.user_id,products.name, count(orders.product_id) as count'))
+                ->leftJoin('products', 'orders.product_id', '=', 'products.id')
+                ->groupBy('orders.user_id', 'products.name')
+                ->get();
+    $products = Product::limit(10)->get();
+    $promotions = Promotion::all();
+
+    $promotion_file = file_get_contents("c:/flink_output/recommendations");
+
+    var_dump($promotion_file);
+
+    return view('index')->with('users',$users)->with('orders',$orders)->with('products',$products)->with('promotions',$promotions);
 });
